e0aec1db33065034c5a78eb830699c27e272cf5c
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-10-09 16:21:21 -0500

    evento al botom y en el Edit Text

--- proy_01_android/app/src/main/java/com/example/myapplication/firstapp/MainActivity.kt
+++ proy_01_android/app/src/main/java/com/example/myapplication/firstapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
@@ -15,13 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val btnStart =findViewById<AppCompatButton>(R.id.btnStart)
+        val etName =findViewById<AppCompatEditText>(R.id.etName)
+
+
+        btnStart.setOnClickListener{
+            Log.i("aristiDev","Boton pulsado con ${etName.text.toString()}")
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        fun prueba(){
-            Log.i("pruena","prueba")
         }
     }
 }
