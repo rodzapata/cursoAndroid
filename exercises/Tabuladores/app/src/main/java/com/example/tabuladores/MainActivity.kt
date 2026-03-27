82e1c01d3cf7a96d5224ce4f88c84fcd449132de
rodzapata <rodneyzapatapalacio@gmail.com>
2026-03-26 19:46:28 -0500

    tabuladores

--- exercises/Tabuladores/app/src/main/java/com/example/tabuladores/MainActivity.kt
+++ exercises/Tabuladores/app/src/main/java/com/example/tabuladores/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -22,6 +31,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tabuladores.ui.theme.TabuladoresTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -36,6 +46,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold() { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding))
                     {
+                        //PagerTabsAnt()
                         PagerTabs()
                     }
 
@@ -48,12 +59,69 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PagerTabs() {
-    val pagerState = rememberPagerState(pageCount = { 3 })
-    val tabs = listOf("Uno", "Dos", "Tres")
+
+    val tabs = listOf(
+        "Uno", "Dos", "Tres","cuatro","cinco",
+        "seis","siete","ocho","nueve","diez","once"
+    )
+
+    val pagerState = rememberPagerState(pageCount = { tabs.size })
+    val scope = rememberCoroutineScope()
+
+    Column {
+
+        ScrollableTabRow(
+            selectedTabIndex = pagerState.currentPage,
+            edgePadding = 8.dp,
+            indicator = { tabPositions ->
+
+                val currentTabPosition = tabPositions[pagerState.currentPage]
+
+                Box(
+                    Modifier
+                        .padding(horizontal = 24.dp)
+                        .fillMaxWidth()
+                ) {
+                    Box(
+                        Modifier
+                            .offset(x = currentTabPosition.left)
+                            .width(currentTabPosition.width)
+                            .height(4.dp) // 👈 más grueso (estilo moderno)
+                            .background(
+                                color = MaterialTheme.colorScheme.primary,
+                                shape = RoundedCornerShape(50)
+                            )
+                    )
+                }
+            }
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = { Text(title) }
+                )
+            }
+        }
+
+        HorizontalPager(state = pagerState) { page ->
+            Text("Página $page", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PagerTabsAnt() {
+    val tabs = listOf("Uno", "Dos", "Tres","cuatro","cinco","seis","siete","ocho","nueve","diez","once")
+    val pagerState = rememberPagerState(pageCount = { tabs.size })
     val scope = rememberCoroutineScope() // 👈 correcto
 
     Column {
-        TabRow(selectedTabIndex = pagerState.currentPage) {
+        ScrollableTabRow(selectedTabIndex = pagerState.currentPage, edgePadding = 8.dp) {
             tabs.forEachIndexed { index, title ->
                 Tab(
                     selected = pagerState.currentPage == index,
@@ -77,6 +145,6 @@ fun PagerTabs() {
 @Composable
 fun GreetingPreview() {
     TabuladoresTheme {
-        PagerTabs()
+        PagerTabsAnt()
     }
 }
